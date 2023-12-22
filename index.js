5e83b5fae42ef4a962ec5d3525980a69b8135818
Niloy11111 <Ektiaruddinniloy859@gmail.com>
2023-12-22 08:04:37 +0600

    get task by it to update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors()) ;
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tg0ohza.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const userCollection = client.db("taskDB").collection("users");
+    const taskCollection = client.db("taskDB").collection("allTask");
 
     app.post('/users', async (req, res) => {
         const user = req.body;
@@ -38,6 +39,62 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result);
       });
+      
+      //get task by userEmail
+       app.get('/tasks', async (req, res) => {
+        let query = {} 
+  
+        if(req.query?.userEmail){
+          query = { userEmail : req.query.userEmail }
+          console.log(query)
+        }
+        
+        const cursor = taskCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+      });
+        //update task
+        app.put('/updateTask/:id', async (req, res) => {
+          const id = req.params.id;
+          const filter = { _id: new ObjectId(id) }
+          const options = { upsert: true };
+          const updateTask = req.body;
+          console.log(updateTask)
+        
+          const task = {
+              $set: {
+                 name : updateTask.name,
+                 priority : updateTask.priority,
+                 postTime : updateTask.postTime,
+                 description : updateTask.description,
+              }
+          }
+        
+          const result = await taskCollection.updateOne(filter, task,options);
+          res.send(result);
+        })
+
+      app.get('/tasks/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await taskCollection.findOne(query);
+        res.send(result);
+      })
+
+        //delete Task
+        app.delete('/tasks/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+          const result = await taskCollection.deleteOne(query);
+          res.send(result);
+        })
+
+      // add task 
+  app.post('/tasks', async(req, res) => {
+    const task = req.body ;
+    const result = await taskCollection.insertOne(task);
+    res.send(result);
+  })
 
 
     await client.db("admin").command({ ping: 1 });
